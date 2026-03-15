b8cc3a8fa0c29c3f03f71a37c8068bb4c6d5aa48
khem Bahadur Lodh <55244922+khemprogrammer@users.noreply.github.com>
2026-03-16 01:11:43 +0545

    Update settings.py

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-your-secret-key-here-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = os.environ.get('*')
 
 
 # Application definition
